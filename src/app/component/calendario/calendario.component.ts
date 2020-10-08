613fa908888c7bf63d5a36ae452c53e2b5f924cf
Luizun <luisacosta360@gmail.com>
2020-10-08 13:17:58 -0500

    Se agregaron Input y Output en calendario
    Se agregaron para poder limpiar el formulario donde esta la fecha y
    poder enviar las fechas deseadas

--- src/app/component/calendario/calendario.component.ts
+++ src/app/component/calendario/calendario.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
   styleUrls: ['./calendario.component.scss']
 })
-export class CalendarioComponent implements OnInit {
+export class CalendarioComponent implements OnInit, OnChanges {
 
   range = new FormGroup({
-    start: new FormControl(),
-    end: new FormControl()
+    start: new FormControl('', [Validators.required]),
+    end: new FormControl('', [Validators.required])
   });
+
+  @Input() limpiar: boolean = false;
+  @Output() rangoFechas: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges() {
+    if (this.limpiar) {
+      this.limpiarForm();
+    }
+  }
+
+  changeFch() {
+    if (this.range.valid) {
+      this.rangoFechas.emit(this.range.value);
+    }
+  }
+
+  limpiarForm() {
+    this.range.reset({
+      start: '',
+      end: ''
+    });
   }
 
 }
