6bdaac23de03ab1184deda6d53642863a659f178
Luizun <luisacosta360@gmail.com>
2020-10-08 15:55:02 -0500

    Se maneja la fecha con localStorage
    Se usa localStorage para guardar la fecha y poder usarla en otros
    componentes

--- src/app/shared/header/contenedor-fecha/contenedor-fecha.component.ts
+++ src/app/shared/header/contenedor-fecha/contenedor-fecha.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FiltrosService } from 'src/app/services/filtros.service';
 
 @Component({
   selector: 'app-contenedor-fecha',
@@ -11,9 +12,21 @@ export class ContenedorFechaComponent implements OnInit {
   limpiar: boolean = false;
   nuevaFecha: any;
 
+  fechaIn: string;
+  fechaFn: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.validarFecha();
+  }
+
+  validarFecha() {
+    if (!JSON.parse(localStorage.getItem('fecha-filtro'))) {
+      localStorage.setItem('fecha-filtro', JSON.stringify({}));
+    }
+    this.fechaIn = JSON.parse(localStorage.getItem('fecha-filtro')).start ? JSON.parse(localStorage.getItem('fecha-filtro')).start : null;
+    this.fechaFn = JSON.parse(localStorage.getItem('fecha-filtro')).end ? JSON.parse(localStorage.getItem('fecha-filtro')).end : null;
   }
 
   getFecha(fecha) {
@@ -22,10 +35,16 @@ export class ContenedorFechaComponent implements OnInit {
 
   btnLimpiar() {
     this.limpiar = true;
-    setTimeout(() => this.limpiar = false, 100);
+    setTimeout(() => {
+      this.limpiar = false;
+      this.tipoSelect = !this.tipoSelect;
+      this.validarFecha();
+    }, 100);
   }
 
   confirmar() {
+    localStorage.setItem('fecha-filtro', JSON.stringify(this.nuevaFecha));
+    this.validarFecha();
     this.tipoSelect = !this.tipoSelect;
   }
 
