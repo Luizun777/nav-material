f116188bb55afdc9c3b1e1b3a820ab4830b1a5b6
Pablo Jose <josepbl808@gmail.com>
2020-10-08 13:32:20 -0500

    make interceptor and config file, set local variable es for i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {
+  HTTP_INTERCEPTORS, HttpClient, HttpClientModule
+} from '@angular/common/http';
+import es from '@angular/common/locales/es';
+import {registerLocaleData} from '@angular/common';
+
+// Dependencies
+import {
+  TranslateLoader, TranslateModule
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { ReactiveFormsModule } from '@angular/forms';
 
 // Materials
@@ -14,23 +25,49 @@ import { TableComponent } from './component/table/table.component';
 import { CalendarioComponent } from './component/calendario/calendario.component';
 import { ContenedorFechaComponent } from './shared/header/contenedor-fecha/contenedor-fecha.component';
 
-@NgModule({
-  declarations: [
+// File Configuration
+import {Config} from './config';
+import {MainHttpInterceptor} from './interceptor/main-http-interceptor';
+
+export const HttpLoaderFactory = (http: HttpClient) => new TranslateHttpLoader(http, Config.translatePath);
+
+const declarations = [
     AppComponent,
     HeaderComponent,
     NavComponent,
     TableComponent,
     CalendarioComponent,
     ContenedorFechaComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialsModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ];
+
+const imports = [
+  BrowserModule,
+  AppRoutingModule,
+  TranslateModule.forRoot({loader: {
+    deps: [HttpClient],
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory
+  }}),
+  BrowserAnimationsModule,
+  MaterialsModule,
+  ReactiveFormsModule
+];
+
+const providers = [{
+  multi: true,
+  provide: HTTP_INTERCEPTORS,
+  useClass: MainHttpInterceptor
+}, {
+  provide: LOCALE_ID,
+  useValue: 'es'
+}];
+
+registerLocaleData(es);
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations,
+  imports,
+  providers
 })
 export class AppModule { }
